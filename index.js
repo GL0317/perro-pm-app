b82d8261e4c5776dd0c83c79fda3ef55070b1953
Gerson Lindor Jr <lindorjr.0317@gmail.com>
2021-01-28 21:14:10 -0800

    re-adjusted the layout, the views are functioning

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const engines = require('consolidate');
 const exphbs = require('express-handlebars');
 const projects = require('./routes/projects');
+// const path = require('path')
 
 
 const app = express();
@@ -12,6 +13,12 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
+// app.use(express.static(path.join(__dirname, '/public')));
+
+
+const prototypeUser = {
+    userId: "5735810494103552"
+}
 
 app.get('/', (req, res)=>{
     const content = {
@@ -22,6 +29,9 @@ app.get('/', (req, res)=>{
 })
 
 app.use('/projects', projects)
+
+// get a list of projects assigned to a user
+app.use('users/:userId/', projects)
 
 app.listen(PORT, ()=> {
     console.log(`Server listening on port ${PORT}`);
--- routes/projects.js
+++ routes/projects.js
@@ -2,12 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) =>{
+    let pageName = "Project List"
     const content = {
-        title: "Project List",
+        title: pageName,
         msg: "The project list page",
-        layout: "page"
+        heading: pageName
     }
     res.render('projectList', content)
+})
+
+router.get('/projects', (req, res) =>{
+    res.send("Nope!!!")
 })
 
 
